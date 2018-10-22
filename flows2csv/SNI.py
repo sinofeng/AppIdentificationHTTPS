fec805b02c7140e1ede3f66490ae000f39a03c9a
maomao1994 <tianmao1994@yahoo.com>
2018-10-22 17:29:24 +0800

    update 20181022 SNI feature

--- flows2csv/SNI.py
+++ flows2csv/SNI.py
@@ -1,8 +1,7 @@
+# coding=utf-8
 import pandas as pd
 import numpy as np
 
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score
 from sklearn.feature_extraction.text import CountVectorizer
 param = {
     'ngram_range' : (1, 1),
@@ -21,5 +20,20 @@ eval_labels=np.asarray(output_val.pop('label'),dtype=np.int32)
 
 train_data=np.asarray(output_train["extension_servername_indication"])
 train_data=c.transform(train_data)
+train_data=train_data.toarray()
 eval_data=np.asarray(output_val["extension_servername_indication"])
 eval_data=c.transform(eval_data)
+eval_data=eval_data.toarray()
+
+# 如何将稀疏矩阵转换为正常的矩阵？？？
+columns=["c_%d"%i for i in range(36)]
+
+train_sni=pd.DataFrame(train_data,columns=columns)
+train_sni.to_csv("./train_sni.csv",index=False)
+del train_data
+del train_sni
+
+eval_sni=pd.DataFrame(eval_data,columns=columns)
+eval_sni.to_csv("./eval_sni.csv",index=False)
+del eval_data
+del eval_sni
