82a1f771bc0cba56487bceb9a8266e30f787b5bd
maomao1994 <tianmao1994@yahoo.com>
2018-10-29 18:41:47 +0800

    第一次运行RNN+CNN组合

--- wide_cnn_rnn/rnn_cnn.py
+++ wide_cnn_rnn/rnn_cnn.py
@@ -13,36 +13,47 @@ from keras.models import Model
 from keras.layers import Embedding
 from keras.layers import SimpleRNN
 from keras.optimizers import Adam
+import config
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+def onehot(x):
+    return np.array(OneHotEncoder().fit_transform(x).todense())
 
-train_data=pd.read_csv("./train.csv",names=['c1','c2','c3','c4','c5','label'])
-val_data=pd.read_csv("./val.csv",names=['c1','c2','c3','c4','c5','label'])
+cnn_train_data=pd.read_csv(config.HTTPS_CONFIG["packet_length_train_path"])
+cnn_val_data=pd.read_csv(config.HTTPS_CONFIG["packet_length_val_path"])
 # print(train_data)
 
+rnn_train_data=pd.read_csv(config.HTTPS_CONFIG["record_type_train_path"])
+rnn_val_data=pd.read_csv(config.HTTPS_CONFIG["record_type_val_path"])
 
-y_train_cnn=train_data.pop("label")
-X_train_cnn = train_data
-y_train_cnn=np.asarray(y_train_cnn)
-X_train_cnn=np.asarray(X_train_cnn).reshape((-1,5,1))
+
+y_train_cnn=cnn_train_data.pop("label")
+X_train_cnn = cnn_train_data.drop(["id"],axis=1)
+y_train_cnn=np.asarray(y_train_cnn).reshape(-1,1)
+y_train_cnn=onehot(y_train_cnn)
+X_train_cnn=np.asarray(X_train_cnn).reshape((-1,128,1))
 
 # print(X_train_rnn)
 
-y_test_cnn=val_data.pop("label")
-X_test_cnn = val_data
-y_test_cnn=np.asarray(y_test_cnn)
-X_test_cnn=np.asarray(X_test_cnn).reshape((-1,5,1))
+y_test_cnn=cnn_val_data.pop("label")
+X_test_cnn = cnn_val_data.drop(["id"],axis=1)
+y_test_cnn=np.asarray(y_test_cnn).reshape(-1,1)
+y_test_cnn=onehot(y_test_cnn)
+X_test_cnn=np.asarray(X_test_cnn).reshape((-1,128,1))
 
 
 
-y_train_rnn=y_train_cnn
-X_train_rnn = train_data
-y_train_rnn=np.asarray(y_train_rnn)
+y_train_rnn=rnn_train_data.pop("label")
+X_train_rnn = rnn_train_data.drop(["id"],axis=1)
+y_train_rnn=np.asarray(y_train_rnn).reshape(-1,1)
+y_train_rnn=onehot(y_train_rnn)
 X_train_rnn=np.asarray(X_train_rnn)
 
 # print(X_train_rnn)
 
-y_test_rnn=y_train_cnn
-X_test_rnn = val_data
-y_test_rnn=np.asarray(y_test_rnn)
+y_test_rnn=rnn_val_data.pop("label")
+X_test_rnn = rnn_val_data.drop(["id"],axis=1)
+y_test_rnn=np.asarray(y_test_rnn).reshape(-1,1)
+y_test_rnn=onehot(y_test_rnn)
 X_test_rnn=np.asarray(X_test_rnn)
 
 batch_size=128
@@ -50,27 +61,27 @@ batch_size=128
 nb_filters=32
 kernel_size=3
 
-cnn_input_shape=(5,1)
+cnn_input_shape=(128,1)
 cnn_inp = Input(shape=cnn_input_shape, dtype='float32', name='cnn')
 # 两层卷积操作
 c = Conv1D(nb_filters, kernel_size=kernel_size,padding='same',strides=1)(cnn_inp)
 c = MaxPooling1D()(c)
 c = Flatten()(c)
-c = Dense(5, activation='relu', kernel_regularizer=l1_l2(l1=0.01, l2=0.01))(c)
+c = Dense(32, activation='relu', kernel_regularizer=l1_l2(l1=0.01, l2=0.01))(c)
 
-rnn_inp= Input(shape=(5,))
-r=Embedding(6,5,input_length=5)(rnn_inp)
-r=SimpleRNN(5)(r)
-r=Dense(5,activation='relu', kernel_regularizer=l1_l2(l1=0.01, l2=0.01))(r)
+rnn_inp= Input(shape=(128,))
+r=Embedding(257,16,input_length=128)(rnn_inp)
+r=SimpleRNN(128)(r)
+r=Dense(32,activation='relu', kernel_regularizer=l1_l2(l1=0.01, l2=0.01))(r)
 
 cr_inp = concatenate([c, r])
 # wide特征和deep特征拼接，wide特征直接和输出节点相连
-cr = Dense(5,activation='relu')(cr_inp)
-cr_out = Dense(1, activation='sigmoid', name='cnn_rnn')(cr)
+cr = Dense(32,activation='relu')(cr_inp)
+cr_out = Dense(17, activation='softmax', name='cnn_rnn')(cr)
 
 # 模型网络的入口和出口
 cr = Model(inputs=[cnn_inp, rnn_inp], outputs=cr_out)
-cr.compile(loss="binary_crossentropy", optimizer="adam",metrics=["accuracy"])
+cr.compile(optimizer=Adam(lr=0.0001),loss="categorical_crossentropy",metrics=["accuracy"])
 # 以下输入数据进行wide and deep模型的训练
 print(cr.summary())
 
