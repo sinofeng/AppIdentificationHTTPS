7279d493c900ebb585aed4052f072fbb98bc6b7e
maomao1994 <tianmao1994@yahoo.com>
2018-11-30 15:37:24 +0800

    update figure acc loss

--- result/loss_acc.py
+++ result/loss_acc.py
@@ -20,8 +20,10 @@ y2=loss_cnn2d["Value"][1:]
 y3=loss_cnn1d_rnn["Value"][1:]
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
+plt.figure(figsize=(8, 12))
 
-plt.figure(figsize=(8, 5))
+ax=plt.subplot(211)
+
 plt.xlabel("Steps")
 plt.ylabel("Loss")
 plt.plot(x, y1, 'r-', mec='k', label='Loss cnn_1d', lw=2)
@@ -30,9 +32,9 @@ plt.plot(x, y3, 'b-',mec='k', label='Loss cnn1d_rnn', lw=2)
 # plt.plot(x, boost, 'm--',mec='k', label='Adaboost Loss',lw=2)
 plt.grid(True, ls='--')
 plt.legend(loc='upper right')
-# plt.title('Loss Function')
-plt.savefig('../result/loss.png')
-plt.show()
+plt.title('(1) Loss over steps',y=-0.18)
+# plt.savefig('../result/loss.png')
+# plt.show()
 
 
 acc_cnn_2d=pd.read_csv("../../data/acc_cnn_2d.csv")
@@ -42,7 +44,8 @@ x_acc_cnn_2d=acc_cnn_2d["Step"]
 y_acc_cnn_2d=acc_cnn_2d["Value"]
 y_acc_cnn_1d=acc_cnn_1d["Value"]
 y_acc_cnn1d_rnn=acc_cnn1d_rnn["Value"]
-plt.figure(figsize=(8, 5))
+plt.subplot(212)
+# plt.figure(figsize=(8, 5))
 plt.xlabel("Steps")
 plt.ylabel("Acc")
 plt.plot(x_acc_cnn_2d, y_acc_cnn_2d, 'r-', mec='k', label='Acc CNN_2D', lw=2)
@@ -51,8 +54,9 @@ plt.plot(x_acc_cnn_2d, y_acc_cnn1d_rnn, 'b-',mec='k', label='Acc CNN1D_RNN', lw=
 # plt.plot(x, boost, 'm--',mec='k', label='Adaboost Loss',lw=2)
 plt.grid(True, ls='--')
 plt.legend(loc='lower right')
-# plt.title('Loss Function')
-plt.savefig('../result/acc.png')
+plt.title('(2) Accuracy over steps',y=-0.18)
+plt.subplots_adjust(hspace=0.25)
+plt.savefig('../result/acc_loss.png',bbox_inches='tight')
 plt.show()
 
 
